d36e61b96ce8e51221eecab521c7a60439eaf5aa
tobiz <pjrobinson@metronet.co.uk>
2019-01-22 21:06:52 +0000

    Ignore this, not used

--- src/flogger_test_sunset.py
+++ src/flogger_test_sunset.py
@@ -14,28 +14,44 @@ import pytz
 
 #class init_sunset(self):
 class init_sunset():
-	def __init__(self, lat, lon, elv, USN_adj):	
+#	def __init__(self, lat, lon, elv, USN_adj):	
+	def __init__(self, location):
 		print "init check_sunset"
-		self.dtnow      = ephem.Observer()
-		self.dtnow.lon  = str(lon) 		#Note that lon should be in string format
-		self.dtnow.lat  = str(lat)      	#Note that lat should be in string format
-		self.dtnow.elev = elv
-		#To get U.S. Naval Astronomical Almanac values, use these settings, USN_adj = -0.34 arcminutes
-		self.dtnow.pressure= 0
-		self.dtnow.horizon = str(USN_adj)
+		self.sun = ephem.Sun()
+		self.twilight = -6 & ephem.degree
 		
-	def is_sunset_now(self): 
-		nowdate     = ephem.localtime(self.dtnow.date)
-		sunrise_nxt = ephem.localtime(self.dtnow.previous_rising(ephem.Sun()))
-		sunset_nxt  = ephem.localtime(self.dtnow.next_setting(ephem.Sun()))
-		print "nowdate is: ", nowdate
-		print "sunrise_nxt is: ", sunrise_nxt
-		print "sunset_nxt is: ", sunset_nxt
-		if (nowdate > sunrise_nxt) and (nowdate < sunset_nxt):
-			return False
-		else:
-		#	print False
+	def is_sunset_now(self, settings, location): 
+		log_datetime = datetime.datetime.now() + datetime.timedelta(hours=int(settings.FLOGGER_LOG_TIME_DELTA))
+		location.date = ephem.Date(log_datetime)
+		print "Ephem date is: ", location.date
+		s = ephem.Sun()
+		s.compute(location)
+		twilight = -6 * ephem.degree    # Defn of Twilight is: Centre of Sun is 6, 12, 18 degrees below horizon (civil, nautical, astronomical)  
+		if s.alt > twilight:
+			print "Is it light at Location? Yes", location, \
+			" Ephem date is: ", ephem.Date(location.date), \
+			" Next sunset at: ", location.next_setting(ephem.Sun())
 			return True
+		else:
+			print "Is it light at Location? No", location, \
+			" Ephem date is: ", ephem.Date(location.date), \
+			" Next sunrise at: ", location.next_rising(ephem.Sun())
+			return False
+            
+                
+		
+		
+#		nowdate     = ephem.localtime(self.dtnow.date)
+#		sunrise_nxt = ephem.localtime(self.dtnow.previous_rising(ephem.Sun()))
+#		sunset_nxt  = ephem.localtime(self.dtnow.next_setting(ephem.Sun()))
+#		print "nowdate is: ", nowdate
+#		print "sunrise_nxt is: ", sunrise_nxt
+#		print "sunset_nxt is: ", sunset_nxt
+#		if (nowdate > sunrise_nxt) and (nowdate < sunset_nxt):
+#			return False
+#		else:
+		#	print False
+#			return True
 			
 #check = init_sunset(54.2289592, -1.20928758608, 238.25, "-0:34")
 #issunset = check.is_sun_set_now()
